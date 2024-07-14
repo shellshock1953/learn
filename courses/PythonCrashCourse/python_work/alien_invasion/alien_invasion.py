ed611b47a93e344435792533ebacb096eab109a2
Bohdan Sukhomlinov <shellshock1953@gmail.com>
2024-07-14 20:28:54 +0300

    prepare end of a game
    Signed-off-by: Bohdan Sukhomlinov <shellshock1953@gmail.com>

--- courses/PythonCrashCourse/python_work/alien_invasion/alien_invasion.py
+++ courses/PythonCrashCourse/python_work/alien_invasion/alien_invasion.py
@@ -81,11 +81,24 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
-        # print(len(self.bullets))
+        self._check_bullet_alien_collisions()
+
+    def _check_bullet_alien_collisions(self):
+        collisions = pygame.sprite.groupcollide(
+            self.bullets,
+            self.aliens,
+            True,
+            True
+        )
+        if not self.aliens:
+            self.bullets.empty()
+            self._create_fleet()
 
     def _update_aliens(self):
         self._check_fleet_edges()
         self.aliens.update()
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            print('SHIP HIT!!!!')
 
     def _fire_bullet(self):
         if len(self.bullets) < self.settings.bullets_allowed:
--- courses/PythonCrashCourse/python_work/alien_invasion/settings.py
+++ courses/PythonCrashCourse/python_work/alien_invasion/settings.py
@@ -6,10 +6,10 @@ class Settings:
         self.bg_color = (57, 117, 165)
         self.ship_speed = 1.5
         self.alien_speed = 1.0
-        self.fleet_drop_speed = 10
+        self.fleet_drop_speed = 1
         self.fleet_direction = 1
 
-        self.bullet_speed = 1.0
+        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
