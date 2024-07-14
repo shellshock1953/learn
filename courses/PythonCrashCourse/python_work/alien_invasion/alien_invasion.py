9060683898fee09a356dfc3a79c761c762bdd149
Bohdan Sukhomlinov <shellshock1953@gmail.com>
2024-07-14 20:14:24 +0300

    fleet moving
    Signed-off-by: Bohdan Sukhomlinov <shellshock1953@gmail.com>

--- courses/PythonCrashCourse/python_work/alien_invasion/alien_invasion.py
+++ courses/PythonCrashCourse/python_work/alien_invasion/alien_invasion.py
@@ -28,6 +28,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
 
@@ -63,6 +64,17 @@ class AlienInvasion:
             self.ship.moving_right = False
         if event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+
+    def _check_fleet_edges(self):
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
     
     def _update_bullets(self):
         self.bullets.update()
@@ -71,6 +83,10 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
         # print(len(self.bullets))
 
+    def _update_aliens(self):
+        self._check_fleet_edges()
+        self.aliens.update()
+
     def _fire_bullet(self):
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
@@ -78,7 +94,25 @@ class AlienInvasion:
 
     def _create_fleet(self):
         alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+        
 
 
 if __name__ == "__main__":
--- courses/PythonCrashCourse/python_work/alien_invasion/settings.py
+++ courses/PythonCrashCourse/python_work/alien_invasion/settings.py
@@ -5,6 +5,9 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (57, 117, 165)
         self.ship_speed = 1.5
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
 
         self.bullet_speed = 1.0
         self.bullet_width = 3
