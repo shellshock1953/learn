a86f20b54527221d63e700e3c312c96a05f27bce
Bohdan Sukhomlinov <shellshock1953@gmail.com>
2024-07-16 11:19:44 +0300

    ships left in Scoreboard is smaller and grayscale
    Signed-off-by: Bohdan Sukhomlinov <shellshock1953@gmail.com>

--- courses/PythonCrashCourse/python_work/alien_invasion/scoreboard.py
+++ courses/PythonCrashCourse/python_work/alien_invasion/scoreboard.py
@@ -56,7 +56,7 @@ class Scoreboard:
     def prep_ships(self):
         self.ships = Group()
         for ship_number in range(self.stats.ships_left):
-            ship = Ship(self.ai_game)
+            ship = Ship(self.ai_game, is_score=True)
             ship.rect.x = 10 + ship_number * ship.rect.width
             ship.rect.y = 10
             self.ships.add(ship)
--- courses/PythonCrashCourse/python_work/alien_invasion/ship.py
+++ courses/PythonCrashCourse/python_work/alien_invasion/ship.py
@@ -5,17 +5,27 @@ from settings import Settings
 
 class Ship(Sprite):
     # TODO: allow ship to move to half to the bottom screen
-    def __init__(self, ai_game) -> None:
+    def __init__(self, ai_game, is_score=False) -> None:
         super().__init__()
         self.screen = ai_game.screen
         self.settings: Settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
-
-        self.image = pygame.image.load("images/ship.png")
+        self.image = self._check_if_score(is_score)
         self.rect = self.image.get_rect()
         self.center_ship()
         self.moving_right = False
         self.moving_left = False
+
+    def _check_if_score(self, is_score):
+        if is_score:
+            return pygame.transform.grayscale(
+                pygame.transform.scale_by(
+                    pygame.image.load("images/ship.png"), 0.5
+                )
+            )
+        else:
+            return pygame.image.load("images/ship.png")
+
 
     def update(self):
         if self.moving_right and self.rect.right < self.screen_rect.right:
